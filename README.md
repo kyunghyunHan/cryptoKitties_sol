1a0899b107a74b7edd51e8c91302c2992351d101
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-06-29 18:43:41 +0900

    feat:KittyCore

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # cryptoKitties_sol
+
+## 기능
+
+- kittyAccessControl:크립토키티 의 관리자 계정설정 및 긴급상황시 크립토키티를 pause/unpause하는 전반적인 관리 기능
+- kittyBase:크립토키티 구조체 정의 및 생성,전송기능
+- kittyOwnerShip:토큰화
+- kittyBreeding:교배기능
+- kittyAuction:옥션과 연동시키는 기능
---
+++ contracts/KittyAccessControl.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract KitttyAccessControl {
+    /*새로운 컨트렉트 주소로 컨트랙트 업그레이드 시 발생하는 이벤트 */
+    event ContractUpgrade(address newContract);
+    /*컨트렉트에서 import 하는 컨트렉트 주소를 세팅해주는 역활의 계정 */
+    address public ceoAddress;
+    /*kittyCore에서 돈을 인출하는 역활의계정 */
+    address public cfoAddress;
+    /*크립토키티의 전반적인 운영에 기여하는 계정 
+    서비스 운영용 함수 호출
+    */
+    address public cooAddress;
+    /*크립토 키티 서비스 중지 여부 표식 */
+    bool public paused = false;
+     
+    /*CEO계정만 호출할수 있도록 해주는 제약자 */
+    modifier onlyCEO() {
+        require(msg.sender == ceoAddress);
+        _;
+    }
+    /*CFO계정만 호출할수 있도록 해주는 제약자 */
+    modifier onlyCFO() {
+        require(msg.sender == cfoAddress);
+        _;
+    } 
+    /*COO계정만 호출할수 있도록 해주는 제약자 */
+    modifier onlyCOO() {
+        require(msg.sender == cooAddress);
+        _;
+    }
+    /*onlyCLevel
+    CEO,CFO,COO계정만 호출할수 있도록 하는 제약자
+     */
+    modifier onlyCLevel() {
+        require(
+            msg.sender == cooAddress ||
+            msg.sender == ceoAddress ||
+            msg.sender == cfoAddress
+        );
+        _;
+    }
+
+    /*CEO계정 세팅함수 CEO계정만 호출가능*/
+    function setCEO(address _newCEO) public onlyCEO {
+        require(_newCEO != address(0));
+
+        ceoAddress = _newCEO;
+    }
+
+    /*CFO계정 세팅함수 CFO계정만 호출가능*/
+    function setCFO(address _newCEO) public onlyCEO {
+        require(_newCEO != address(0));
+
+        ceoAddress = _newCEO;
+    }
+
+    /*COO계정 세팅함수 COO계정만 호출가능*/
+    function setCOO(address _newCEO) public onlyCEO {
+        require(_newCEO != address(0));
+
+        ceoAddress = _newCEO;
+    }
+
+    /*KittuCore컨트렉트에 쌓인 ETH를 CFO계정으로 인출하는 함수 CFO계정만 호출가능 */
+    function withdrawBalance() external onlyCFO {
+        // cfoAddress.transfer(this.balance);
+        (bool success, ) = payable(cfoAddress).call{value: address(this).balance}("");
+        require(success, "Failed to send Ether");
+    }
+    /*KittyCore가 paused상태가 아닌 경우에만 호출가능하도록 하는 제약자 */    
+    modifier whenNotPaused() {
+        require(!paused);
+        _;
+    }
+    /*KittyCore가 Paused상태일떄만 호출가능하도록 하는 제약자 */
+    modifier whenPaused {
+         require(paused);
+         _;
+     }
+
+}
